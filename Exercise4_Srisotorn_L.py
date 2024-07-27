1296545908ca1a4979f5e91b2f6a7460fccf4a40
Samiszen <101392933+Samiszen@users.noreply.github.com>
2024-07-27 19:55:19 +0700

    Create Exercise4_Srisotorn_L.py

---
+++ Exercise4_Srisotorn_L.py
@@ -0,0 +1,12 @@
+#Expected output
+FE = 95.5
+GE = 59
+CS = 82.5
+CP = 72
+
+print("--- Your Score ---"
+print("Foundation English :",               FE)
+print("General Business   :",               GE)
+print("Introduction to Computer Systems:",  CS)
+print("Computer Programming :",             CP)
+      
