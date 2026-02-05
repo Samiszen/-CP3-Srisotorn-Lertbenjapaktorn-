91cbcd0cf55418f350fc4a3cc2e913d347cf58a3
Samiszen <101392933+Samiszen@users.noreply.github.com>
2026-02-05 11:39:10 +0700

    Update Exercise4_Srisotorn_L.py

--- Exercise4_Srisotorn_L.py
+++ Exercise4_Srisotorn_L.py
@@ -4,7 +4,7 @@ GE = 59
 CS = 82.5
 CP = 72
 
-print("--- Your Score ---"
+print("--- Your Score ---")
 print("Foundation English :",               FE)
 print("General Business   :",               GE)
 print("Introduction to Computer Systems:",  CS)
